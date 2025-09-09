49b8e6f7bc9c7f3c2e9f86021d5345fc2e43b797
Fabiano de Albuquerque <fabianodealbuquerque@outlook.com>
2025-09-09 20:19:17 -0300

    Subindo Dockerfile e app.py

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-print("Hello World - Fabiano de Albuquerque")
+# app.py
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello World! Jenkins + Docker funcionando!"
+
+if __name__ == "__main__":
+    # Host 0.0.0.0 permite acesso externo ao container
+    # Porta 8080, que é mapeada no Docker
+    app.run(host="0.0.0.0", port=8080)
