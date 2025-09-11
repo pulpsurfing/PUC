b6ac994193723440c338fd5a88a7ca4013d8cb2e
Fabiano de Albuquerque <fabianodealbuquerque@outlook.com>
2025-09-10 21:48:30 -0300

    Subindo Dockerfile e app.py

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
-# app.py
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello World! Jenkins + Docker funcionando!"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     # Host 0.0.0.0 permite acesso externo ao container
